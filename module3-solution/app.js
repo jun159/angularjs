205a24b541255dc234412d3c34ba85a39a9d6a88
jun159 <baojun159@gmail.com>
2020-03-08 11:49:42 +0800

    Update Narrow Menu List functionalities

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -30,6 +30,7 @@
     narrowCtrl.foundItems = MenuSearchService.getFoundItems();
 
     narrowCtrl.getMatchedMenuItems = function(searchTerm) {
+      MenuSearchService.resetFoundItems();
       if(searchTerm) {
         MenuSearchService.getMatchedMenuItems(searchTerm);
       }
@@ -52,7 +53,6 @@
         url: (ApiBasePath + "/menu_items.json")
       }).then(function(response) {
         var menuItems = response.data.menu_items;
-        foundItems.length = 0;
         menuItems.map(function(item) {
           if(item.description.includes(searchTerm)) {
             foundItems.push(item);
@@ -61,6 +61,10 @@
 
         return foundItems;
       });
+    }
+
+    service.resetFoundItems = function() {
+      foundItems.length = 0;
     }
 
     service.getFoundItems = function() {
